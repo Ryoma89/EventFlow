9a0a21191a3845ffbfc41400bb043fe791913509
Kento-Kuki <144260223+Kento-Kuki@users.noreply.github.com>
2024-07-29 19:30:20 -0700

    modify eventRoutes

--- backend/routes/eventRoutes.ts
+++ backend/routes/eventRoutes.ts
@@ -5,6 +5,7 @@ import {
   editEvent,
   deleteEvent,
   getAllEvents,
+  getEventById,
 } from '../controllers/eventController';
 
 import authenticateJWT from '../middleware/authMiddleware';
@@ -12,6 +13,7 @@ import authenticateJWT from '../middleware/authMiddleware';
 const router = Router();
 
 router.get('/events', getAllEvents);
+router.get('/events/:eventId', getEventById);
 router.post('/events', authenticateJWT, createEvent);
 router.put('/events', authenticateJWT, editEvent);
 router.delete('/events', authenticateJWT, deleteEvent);
