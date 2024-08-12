4ae26c231c25f8a84b1c6036e7d106219f778e3b
Ryoma <Ryoma3540@gmail.com>
2024-08-12 12:52:38 -0700

    fix index.ts

--- backend/index.ts
+++ backend/index.ts
@@ -21,7 +21,6 @@ const app = express();
 const port = Number(process.env.PORT) || 5000;
 
 app.use(cookieParser());
-app.use(express.json());
 app.use(
   cors({
     origin: process.env.FRONTEND_URL || 'http://localhost:3000',
@@ -31,6 +30,7 @@ app.use(
   })
   );
   app.use('/api/stripe', stripeRoutes);
+  app.use(express.json());
   
 app.use('/api', authRoutes);
 app.use('/api', userRoutes);
