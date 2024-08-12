319c0123a52df467efc12b41b56c41d9ec3fc85a
Ryoma <Ryoma3540@gmail.com>
2024-08-12 13:19:12 -0700

    fix index.ts

--- backend/index.ts
+++ backend/index.ts
@@ -21,8 +21,9 @@ if (!process.env.FRONTEND_URL) {
 const app = express();
 const port = Number(process.env.PORT) || 5000;
 
-app.use(cookieParser());
 app.post('/api/stripe/webhook', express.raw({type: 'application/json'}), stripeWebhook);
+
+app.use(cookieParser());
 app.use(express.json());
 app.use(
   cors({
