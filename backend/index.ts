9b2765ae210bf9c7350f71d1daea5136dcbbd383
Ryoma <Ryoma3540@gmail.com>
2024-08-12 00:35:22 -0700

    fixed position(index.ts) & delete console

--- backend/index.ts
+++ backend/index.ts
@@ -21,7 +21,6 @@ const app = express();
 const port = Number(process.env.PORT) || 5000;
 
 app.use(cookieParser());
-app.use(express.json());
 app.use(
   cors({
     origin: process.env.FRONTEND_URL || 'http://localhost:3000',
@@ -30,9 +29,9 @@ app.use(
     credentials: true,
   })
   );
-  
-app.use('/api/stripe', stripeRoutes);
+app.use('/api/stripe', stripeRoutes);app.use(express.json());
 app.use('/api', authRoutes);
+
 app.use('/api', userRoutes);
 app.use('/api', eventRoutes);
 app.use('/api', commentRoutes);
